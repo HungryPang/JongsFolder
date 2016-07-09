76c1da4104de2266c68004f5310c6ccbc9c05a31
J.H.Chae <jongsys21@naver.com>
2016-07-09 23:06:11 +0900

    PlayScene 설계 중

---
+++ HungryPang_Jongs/Assets/Scripts/FoodZone.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodZone : MonoBehaviour {
+    public FoodSlot slot;
+    public int zoneWidth = 8;
+    public int zoneHeight = 8;
+
+    public FoodSlot[,] slotList = null;
+
+    // Use this for initialization
+    void Awake()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        Vector2 pos = transform.position;
+        Vector2 size = Vector2.Scale(collider.size * 0.5f, transform.localScale);
+        Vector2 offset = pos - size + new Vector2(0.5f, 0.5f);
+        Vector2 pitch = new Vector2(size.x / zoneWidth, size.y / zoneHeight) * 2;
+
+        Vector2 temp = new Vector2();
+        slotList = new FoodSlot[zoneWidth, zoneHeight];
+
+        for (int x = 0; x < zoneWidth; ++x)
+        {
+            for (int y = 0; y < zoneHeight; ++y)
+            {
+                FoodSlot newSlot = Instantiate(slot);
+                temp.x = pitch.x * x;
+                temp.y = pitch.y * y;
+                newSlot.transform.parent = transform;
+                newSlot.transform.position = offset + temp;
+                slotList[x, y] = newSlot;
+            }
+        }
+
+    }
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HungryPang_Jongs/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FoodSystem
+{
+    public enum FoodTypes
+    {
+        eFoodCheese = 0,
+        eFoodAmond,
+        eFoodApple,
+        eFoodAnt,
+        eFoodRice,
+        eFoodMouse,
+        eFoodLeaf,
+        eFoodAcorn,
+        eFoodHay, // 건초
+        eFoodFeed,
+        eFoodCow,
+        eFoodChicken,
+        eFoodFire,
+        eFoodEgg,
+        eFoodCarrot,
+        eFoodHorse,
+        eFoodPaper,
+        eFoodSeed,
+        eFoodSheep,
+        eFoodBanana,
+        eFoodBread,
+        eFoodCookedRice, // 밥
+        eFoodLarva, // 애벌래
+        eFoodbarley,
+        eFoodTypesNum
+    }
+
+    public struct FoodData
+    {
+        public FoodTypes type;
+        public int score;
+
+        public FoodData(FoodTypes typeVal, int scoreVal)
+        {
+            type = typeVal;
+            score = scoreVal;
+        }
+    }
+
+    public class FoodManager
+    {
+        static FoodManager pointer = null;
+        FoodData[] foodTypeList = null;
+
+        public static FoodManager getInstance()
+        {
+            if (null == pointer)
+            {
+                pointer = new FoodManager();
+                pointer.setting();
+            }
+            return pointer;
+        }
+
+        public void setting()
+        {
+            foodTypeList = new FoodData[(int)FoodTypes.eFoodTypesNum];
+            foodTypeList[(int)FoodTypes.eFoodCheese] = new FoodData(FoodTypes.eFoodCheese, 800);
+        }
+    }
+}
+
+public class GameSystem : MonoBehaviour {
+    string[] resourcesNameHashArray = new string[10];
+    FoodZone foodzone = null;
+    int foodtypeNum = 0;
+    // Use this for initialization
+
+    LinkedList<FoodSystem.FoodTypes> foodlist;
+
+	void Start () {
+        for (int i = 0; i < 10; ++i)
+        {                                                       
+            resourcesNameHashArray[i] = "number_" + i;
+        }
+
+        foodtypeNum = 9; //(int)FoodSystem.FoodTypes.eFoodTypesNum - 1;
+        for (int i = 0; i < 1000; ++i)
+        {
+            int val = Random.Range(0, foodtypeNum);
+            foodlist.AddLast((FoodSystem.FoodTypes) val);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
